0eef9f864761422a585f4ba3eb73e9cf3d7aab90
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-12-02 15:09:49 +0100

    Add day2 functionality with implementation of part1

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -6,27 +6,84 @@
 #include <sstream>
 #include <algorithm>
 
-void part1(){
-    return;
+bool part1(std::vector<int> row){
+    int current{0}, last{0};
+    bool first{true};
+    bool increasing{true};
+    
+    //Check of the current row is increasing or decreasing
+    if(row[0] > row[1])
+        increasing = false;
+
+    for(auto a : row){
+        //std::cout << "last " << last << " - current: " << current << std::endl;
+
+        if(first)
+        {
+            first = false;
+            last = a;
+            continue;
+        }
+
+        current = a;
+
+        if(increasing){
+            if(current <= last)
+                return false;
+        } else{
+            if(current >= last)
+                return false;
+        }
+
+        if(abs(current-last) > 3 )
+        {
+            //std::cout << "---" << last << " and " << current << " --- false" << std::endl;
+            return false;
+        }
+
+        last = current; 
+    }
+    return true;
 }
 
 void part2(){
     return;
 }
 
-void day1() {
-    std::ifstream input("../day2/testInputDay2.txt");
+void day2() {
+    std::ifstream input("../day2/inputDay2.txt");
 
     if (!input) {
         std::cerr << "Failed to open file." << std::endl;
         return ;
     }
-    
+
     std::string line;
+    int sum{};
 
     while (std::getline(input, line)) {
+        //std::cout << "\n Line: " << line << std::endl;
+        std::vector<int> row{};
         std::stringstream ss;
+        std::string temp;
+        int found;
+
+        ss << line;
+
+        while (!ss.eof()) {
+    
+            ss >> temp;
+    
+            if (std::stringstream(temp) >> found){
+                row.push_back(found);
+            }
+            temp = "";
+        }
+        
+        if(part1(row))
+            sum++;
     }
 
+    std::cout << "Part 1: " << sum << std::endl;
     return;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "day1/day1.h"
 #include "day2/day2.h"
 
