cb7bf2e323c751cb8e75bee7b870fe0b336da8e5
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-12-03 15:38:15 +0100

    finished part ,1 day 3

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -4,20 +4,61 @@
 #include "day3.h"
 #include "../functions.h"
 #include <iostream>
-#include <vector>
 #include <string>
-#include <sstream>
-#include <algorithm>
+#include <regex>
 
-bool part1() {
+int part1(std::string line)
+{
+    int sum{};
+    std::string newLine;
+    for (auto &a : line)
+    {
+        if (a == 'm')
+            newLine += ' ';
+        newLine += a;
+        if (a == ')')
+            newLine += ' ';
+    }
+
+    std::regex matchExpression("mul\\(([1-9]\\d*),([1-9]\\d*)\\)");
+
+    std::smatch matches;
+    std::string::const_iterator searchStart(newLine.cbegin());
+    while (std::regex_search(searchStart, newLine.cend(), matches, matchExpression))
+    {
+        std::cout << "match: " << matches.str() << std::endl; 
+        std::string current = matches.str();
+        if (matches.size() == 3)
+        { // Ensure there are 3 matches: the whole match and two groups
+            int firstInt = std::stoi(matches[1].str());
+            int secondInt = std::stoi(matches[2].str());
+            int product = firstInt * secondInt;
+            sum += product; // Accumulate the product            }
+
+            searchStart = matches.suffix().first;
+        }
+    }
+
+    return sum;
 }
 
-bool part2() {
+bool part2()
+{
+    return true;
 }
 
-void day3() {
+void day3()
+{
     std::string fileURL = "../day3/inputDay3.txt";
     std::ifstream input = readFile(fileURL);
 
+    std::string line;
+    int sum{};
 
+    while (std::getline(input, line))
+    {
+        sum += part1(line);
+    }
+
+    std::cout << "part 1: " << sum << std::endl;
 }
