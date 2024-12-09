6cdfc979aa909d80ee6ba9954af10ffbd1032d01
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-12-09 09:49:36 +0100

    Day 7 part 1

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -7,9 +7,36 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 
-void part1() {
+long part1(std::vector<std::pair<long, std::vector<long>>>& equations) {
+    long sum{};
+    for(auto& equation : equations){
+        long length = equation.second.size();
+        std::vector<long> results;
+
+        for(auto& term : equation.second){
+
+            if(results.size() == 0){
+                results.push_back(term);
+            } else {
+                std::vector<long> temp;
+                for (auto result : results){
+                    long mult = result*term;
+                    long add = result + term;
+                    temp.push_back(mult);
+                    temp.push_back(add);
+                }
+                results = temp;
+            }
+        }
+        auto pos = find(results.begin(), results.end(), equation.first);
+        if (pos != results.end()) {
+            sum += equation.first;
+        }
+    }
+    return sum;
 }
 
 void part2() {
@@ -17,51 +44,38 @@ void part2() {
 
 void day7()
 {
-    std::string fileURL = "../day7/testInputDay7.txt";
+    std::string fileURL = "../day7/inputDay7.txt";
     std::ifstream input = readFile(fileURL);
 
     std::string line;
-    int sum1{}, sum2{};
+    long sum1{}, sum2{};
 
-    std::vector<std::pair<int, std::vector<int>>> equations;
+    std::vector<std::pair<long, std::vector<long>>> equations;
 
     while (std::getline(input, line)){
-        std::pair<int, std::vector<int>> temp;
-        std::vector<int> args;
-        std::stringstream ss(line);
-        int found;
-
+        std::pair<long, std::vector<long>> temp;
+       
         size_t pos = line.find(':');
 
         std::string first = line.substr(0, pos);
+        temp.first = std::stoul(first);
 
-        std::string second = line.substring(pos,);
+        long length = line.size();
 
-
-        ss >> found;
-
-        temp.first = found;
-
-
-
-       
-
+        std::string second = line.substr(pos+1, length-pos);
         
-
-
+        std::stringstream ss(second);
+        long found;
 
         while (ss >> found) {
-            args.push_back(found);
-            std::cout << "arg: " << found << std::endl; 
+            temp.second.push_back(found);
         }
-
-        temp.second = args;
 
         equations.push_back(temp);
     }
     input.close();
 
-/*
+     /*
     for (auto equation : equations){
         std::cout << "sum: " << equation.first << "  operands: " << equation.second.size() << std::endl;
 
@@ -69,7 +83,10 @@ void day7()
             std::cout << "- " << term << std::endl;
         }
     }
-*/
+    */
+    
+    
+    sum1 = part1(equations);
 
 
     std::cout << "part 1: " << sum1 << std::endl;
