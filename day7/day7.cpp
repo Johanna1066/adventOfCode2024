78ad393c5a1a782b1ebbcd0b2040d3f23ecec80b
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-12-09 09:10:00 +0100

    day 7 not finished

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -5,6 +5,9 @@
 #include "../functions.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+
 
 void part1() {
 }
@@ -12,18 +15,62 @@ void part1() {
 void part2() {
 }
 
-void dayX()
+void day7()
 {
-    std::string fileURL = "../day7/inputDay7.txt";
+    std::string fileURL = "../day7/testInputDay7.txt";
     std::ifstream input = readFile(fileURL);
 
     std::string line;
     int sum1{}, sum2{};
 
+    std::vector<std::pair<int, std::vector<int>>> equations;
+
     while (std::getline(input, line)){
+        std::pair<int, std::vector<int>> temp;
+        std::vector<int> args;
+        std::stringstream ss(line);
+        int found;
+
+        size_t pos = line.find(':');
+
+        std::string first = line.substr(0, pos);
+
+        std::string second = line.substring(pos,);
+
+
+        ss >> found;
+
+        temp.first = found;
+
+
+
+       
+
         
+
+
+
+        while (ss >> found) {
+            args.push_back(found);
+            std::cout << "arg: " << found << std::endl; 
+        }
+
+        temp.second = args;
+
+        equations.push_back(temp);
     }
     input.close();
+
+/*
+    for (auto equation : equations){
+        std::cout << "sum: " << equation.first << "  operands: " << equation.second.size() << std::endl;
+
+        for (auto term : equation.second){
+            std::cout << "- " << term << std::endl;
+        }
+    }
+*/
+
 
     std::cout << "part 1: " << sum1 << std::endl;
     std::cout << "part 2: " << sum2 << std::endl;
